6821a995b57cfc165c0348b7400ca709431d4da5
Kai Page <kaibug@gmail.com>
2019-03-07 17:36:19 -0600

    Basic operations done for student controller

--- lib/controllers/students_controller.dart
+++ lib/controllers/students_controller.dart
@@ -19,4 +19,33 @@ class StudentsController extends ResourceController {
     }
     return Response.ok(student);
   }
+
+  @Operation.put("id")
+  Future<Response> putStudentById(
+      @Bind.path("id") int id, @Bind.body() DbStudent student) async {
+    if (student.id != null && student.id != id) {
+      return Response.badRequest(body: "ID in url and body don't match");
+    }
+
+    final query = Query<DbStudent>(_context)
+      ..where((s) => s.id).equalTo(id)
+      ..values = student;
+    query.values.id = id;
+    if (await query.fetchOne() != null) {
+      return Response.ok(await query.updateOne());
+    } else {
+      return Response.ok(await query.insert());
+    }
+  }
+
+  @Operation.delete("id")
+  Future<Response> deleteStudentById(@Bind.path("id") int id) async {
+    final query = Query<DbStudent>(_context)..where((s) => s.id).equalTo(id);
+    final numDeleted = await query.delete();
+    if (numDeleted > 0) {
+      return Response.ok(null);
+    } else {
+      return Response.notFound();
+    }
+  }
 }
